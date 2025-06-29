db483e384feee582c4ef3abb8cd05ad3952aab07
Lasikiewicz <jason@sam-tech.co.uk>
2025-06-29 19:27:42 +0100

    fix: Display content and add layout toggle

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,14 @@
-// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// See: https://firebase.google.com/docs/web/learn-more#config-object
+// This configuration object reads the credentials from your .env.local file.
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
